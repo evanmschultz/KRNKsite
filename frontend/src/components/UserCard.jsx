d78401b196da502e59c37ae2ed76ca09ae6e49d5
David Kral <dockral@gmail.com>
2023-09-14 20:12:43 -0700

    Testing receiving from backend.

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 function UserCard() {
+    const [user, setUser] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+    })
+
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/v1/users/1') //Test case, will need to change to dynamic
+            .then(res => setUser(res.data))
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }, [])
+
+
     return (
-        <div>UserCard</div>
+        <div>
+            UserCard
+            <h1>Test of {user.first_name} </h1>
+            {user.last_name}
+            {user.email}
+        </div>
     )
 }
 
