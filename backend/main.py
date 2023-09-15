e5d536167a416e057bb86363b6460bed05a903cc
Evan <evanmschultz@gmail.com>
2023-09-14 19:26:27 -0600

    Adds middleware

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,45 @@
 from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine
 from config.database import Base, SessionLocal, engine
 
 # Initialize FastAPI App
 app = FastAPI()
+
+
+# Debug middleware for printing headers should be the first middleware
+@app.middleware("http")
+async def debug_headers(request: Request, call_next):
+    print("Incoming Request Headers:", request.headers)
+    return await call_next(request)
+
+
+# Middleware to manage database sessions
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next) -> Response:
+    """
+    Middleware to manage database sessions.
+    """
+    response: Response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
+
+
+# CORS Middleware settings
+origins = [
+    "http://localhost:3000",  # The origin where your frontend runs
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from app.models.user import User
 from app.models.topic import Topic
@@ -15,34 +51,6 @@ from app.schemas.user_schema import UserCreateSchema, UserResponseSchema
 # Create the database tables
 Base.metadata.create_all(bind=engine)
 
-
-@app.middleware("http")
-async def db_session_middleware(request: Request, call_next) -> Response:
-    """
-    Middleware to manage database sessions.
-
-    This middleware attaches a new SQLAlchemy session to each incoming HTTP request
-    by storing it in `request.state.db`. The session is closed when the request is
-    finished. This ensures that you can access the DB session by referencing `request.state.db`
-    within the scope of a request.
-
-    Args:
-        request (Request): The incoming request object provided by FastAPI.
-        call_next: A function to call the next middleware or route.
-
-    Returns:
-        Response: The outgoing response object.
-    """
-    response: Response = Response("Internal server error", status_code=500)
-    try:
-        request.state.db = SessionLocal()
-        response = await call_next(request)
-    finally:
-        request.state.db.close()
-    return response
-
-
-# Import your routers here
 from app.routes.users import router as user_router
 
 # Include the routers in the app
