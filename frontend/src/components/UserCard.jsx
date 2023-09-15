624c412cdea720b80e623bb44888b8a940295884
Evan <evanmschultz@gmail.com>
2023-09-15 00:38:09 -0600

    Updates axios to address CORS issue

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,31 +1,30 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function UserCard() {
-    const [user, setUser] = useState({
-        first_name: "",
-        last_name: "",
-        email: "",
-        password: "",
-    })
+	const [user, setUser] = useState({
+		first_name: '',
+		last_name: '',
+		email: '',
+		password: ''
+	});
 
+	useEffect(() => {
+		axios
+			.get('http://localhost:8000/v1/users/1') //Test case, will need to change to dynamic
+			.then((res) => setUser(res.data))
+			.then((res) => console.log(res))
+			.catch((err) => console.log(err));
+	}, []);
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/api/v1/users/1') //Test case, will need to change to dynamic
-            .then(res => setUser(res.data))
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
-    }, [])
-
-
-    return (
-        <div>
-            UserCard
-            <h1>Test of {user.first_name} </h1>
-            {user.last_name}
-            {user.email}
-        </div>
-    )
+	return (
+		<div>
+			UserCard
+			<h1>Test of {user.first_name} </h1>
+			{user.last_name}
+			{user.email}
+		</div>
+	);
 }
 
-export default UserCard
+export default UserCard;
--- frontend/src/components/UserForm/UserForm.jsx
+++ frontend/src/components/UserForm/UserForm.jsx
@@ -1,99 +1,194 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import styles from './UserForm.module.css';
-import { Button, FormControl, Input, InputLabel, InputAdornment} from "@mui/material";
-import axios from "axios";
+import {
+	Button,
+	FormControl,
+	Input,
+	InputLabel,
+	InputAdornment
+} from '@mui/material';
+import axios from 'axios';
 
 const UserForm = (props) => {
-    const {onSubmitProp, type} = props;
-    const [user, setUser] = useState({
-        first_name: "",
-        last_name: "",
-        email: "",
-        password: "",
-        confirm_password: ""
-    })
-    const [showPassword, setShowPassword] = useState(false);
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        try {
-            if (type === "register") {
-                // Handle registration form submission
-                await onSubmitProp(user);
-    
-                const response = await axios.post('http://localhost:8000/api/v1/users/register', user);
-                console.log(response);
-    
-                // Reset the user state or perform other actions specific to registration
-                setUser({
-                    first_name: "",
-                    last_name: "",
-                    email: "",
-                    password: "",
-                    confirm_password: ""
-                });
-            } else if (type === "login") {
-                const response = await axios.post('http://localhost:8000/api/v1/users/login', user);
-                console.log(response);
-                // Handle login form submission
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    
-    
+	const { onSubmitProp, type } = props;
+	const [user, setUser] = useState({
+		first_name: '',
+		last_name: '',
+		email: '',
+		password: '',
+		confirm_password: ''
+	});
+	const [showPassword, setShowPassword] = useState(false);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		onSubmitProp(user);
+		setUser({
+			first_name: '',
+			last_name: '',
+			email: '',
+			password: '',
+			confirm_password: ''
+		});
+	};
 
-
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                    <div className={styles.container}>
-                        {
-                            type == "register" ?
-                            <>
-                                <FormControl>
-                                    <InputLabel htmlFor={"first_name" + type} style={{color: 'white', marginTop: "5px"}}>First Name: </InputLabel>
-                                    <Input type="text" className={styles.field} id={"first_name" + type} value={user.first_name} style={{color: 'white'}} onChange={(e) => setUser((prev) => ({...prev, first_name: e.target.value}))}/>
-                                </FormControl>
-                                <FormControl>
-                                    <InputLabel htmlFor={"last_name" + type} style={{color: 'white', marginTop: "5px"}}>Last Name: </InputLabel>
-                                    <Input type="text" className={styles.field} id={"last_name" + type} value={user.last_name} style={{color: 'white'}} onChange={(e) => setUser((prev) => ({...prev, last_name: e.target.value}))}/>
-                                </FormControl>
-                            </>:
-                            ""
-                        }
-                        <FormControl>
-                            <InputLabel htmlFor={"Email" + type} style={{color: 'white', marginTop: "5px"}}>Email: </InputLabel>
-                            <Input type="email" className={styles.field} id={"Email" + type} value={user.email} style={{color: 'white'}} onChange={(e) => setUser((prev) => ({...prev, email: e.target.value}))}/>
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor={"password" + type} style={{color: 'white', marginTop: "5px"}}>Password: </InputLabel>
-                            <Input type={showPassword ? "text" : "password"} className={styles.field} id={"password" + type} value={user.password} style={{color: 'white'}} onChange={(e) => setUser((prev) => ({...prev, password: e.target.value}))}
-                            endAdornment={
-                                    <InputAdornment className={styles.eye} position="end">
-                                        <i className={showPassword ? `far fa-eye-slash ${styles.eye}`: `far fa-eye ${styles.eye}`} id="toggleShowPassword" onClick={() => setShowPassword(prev => !prev)}></i>
-                                    </InputAdornment>
-                                }/>
-                            
-                        </FormControl>
-                        {
-                            type == "register" ?
-                            <FormControl>
-                                <InputLabel htmlFor={"confirm_password" + type} style={{color: 'white', marginTop: "5px"}}>Confirm Password: </InputLabel>
-                                <Input type={showPassword ? "text" : "password"} className={styles.field} id={"confirm_password" + type} value={user.confirm_password} style={{color: 'white'}} onChange={(e) => setUser((prev) => ({...prev, confirm_password: e.target.value}))}/>
-                            </FormControl> :
-                            ""
-                        }
-                    </div>
-                    {
-                        type == "register" ?
-                        <Button variant="outlined" type="submit" style={{color: "white", border: "1px solid white"}}>Sign Up</Button> :
-                        <Button variant="outlined" type="submit" style={{color: "white", border: "1px solid white"}}>Log In</Button>
-                    }
-            </form>
-        </>
-    );
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<div className={styles.container}>
+					{type == 'register' ? (
+						<>
+							<FormControl>
+								<InputLabel
+									htmlFor={'first_name' + type}
+									style={{ color: 'white', marginTop: '5px' }}
+								>
+									First Name:{' '}
+								</InputLabel>
+								<Input
+									type='text'
+									className={styles.field}
+									id={'first_name' + type}
+									value={user.first_name}
+									style={{ color: 'white' }}
+									onChange={(e) =>
+										setUser((prev) => ({
+											...prev,
+											first_name: e.target.value
+										}))
+									}
+								/>
+							</FormControl>
+							<FormControl>
+								<InputLabel
+									htmlFor={'last_name' + type}
+									style={{ color: 'white', marginTop: '5px' }}
+								>
+									Last Name:{' '}
+								</InputLabel>
+								<Input
+									type='text'
+									className={styles.field}
+									id={'last_name' + type}
+									value={user.last_name}
+									style={{ color: 'white' }}
+									onChange={(e) =>
+										setUser((prev) => ({
+											...prev,
+											last_name: e.target.value
+										}))
+									}
+								/>
+							</FormControl>
+						</>
+					) : (
+						''
+					)}
+					<FormControl>
+						<InputLabel
+							htmlFor={'Email' + type}
+							style={{ color: 'white', marginTop: '5px' }}
+						>
+							Email:{' '}
+						</InputLabel>
+						<Input
+							type='email'
+							className={styles.field}
+							id={'Email' + type}
+							value={user.email}
+							style={{ color: 'white' }}
+							onChange={(e) =>
+								setUser((prev) => ({
+									...prev,
+									email: e.target.value
+								}))
+							}
+						/>
+					</FormControl>
+					<FormControl>
+						<InputLabel
+							htmlFor={'password' + type}
+							style={{ color: 'white', marginTop: '5px' }}
+						>
+							Password:{' '}
+						</InputLabel>
+						<Input
+							type={showPassword ? 'text' : 'password'}
+							className={styles.field}
+							id={'password' + type}
+							value={user.password}
+							style={{ color: 'white' }}
+							onChange={(e) =>
+								setUser((prev) => ({
+									...prev,
+									password: e.target.value
+								}))
+							}
+							endAdornment={
+								<InputAdornment
+									className={styles.eye}
+									position='end'
+								>
+									<i
+										className={
+											showPassword
+												? `far fa-eye-slash ${styles.eye}`
+												: `far fa-eye ${styles.eye}`
+										}
+										id='toggleShowPassword'
+										onClick={() =>
+											setShowPassword((prev) => !prev)
+										}
+									></i>
+								</InputAdornment>
+							}
+						/>
+					</FormControl>
+					{type == 'register' ? (
+						<FormControl>
+							<InputLabel
+								htmlFor={'confirm_password' + type}
+								style={{ color: 'white', marginTop: '5px' }}
+							>
+								Confirm Password:{' '}
+							</InputLabel>
+							<Input
+								type={showPassword ? 'text' : 'password'}
+								className={styles.field}
+								id={'confirm_password' + type}
+								value={user.confirm_password}
+								style={{ color: 'white' }}
+								onChange={(e) =>
+									setUser((prev) => ({
+										...prev,
+										confirm_password: e.target.value
+									}))
+								}
+							/>
+						</FormControl>
+					) : (
+						''
+					)}
+				</div>
+				{type == 'register' ? (
+					<Button
+						variant='outlined'
+						type='submit'
+						style={{ color: 'white', border: '1px solid white' }}
+					>
+						Sign Up
+					</Button>
+				) : (
+					<Button
+						variant='outlined'
+						type='submit'
+						style={{ color: 'white', border: '1px solid white' }}
+					>
+						Log In
+					</Button>
+				)}
+			</form>
+		</>
+	);
 };
 
 export default UserForm;
