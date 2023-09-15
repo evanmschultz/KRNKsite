5af6dd20e3628d1eac97bc0b48d9f548ef50a839
Kyle-Rachman <kcrachman@gmail.com>
2023-09-15 14:51:19 -0700

    Little more article styling

--- frontend/src/components/Article/Article.jsx
+++ frontend/src/components/Article/Article.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import styles from './Article.module.css';
+import { useParams } from 'react-router-dom';
 
 const Article = (props) => {
+    const { id } = useParams();
+
+    // TODO: make axios get request for article information
+
     return (
         <>
             <Navbar></Navbar>
             <div className={styles.content}>
-                <h1>Article</h1>
+                <h1>Title</h1>
+                <hr />
+                <p>A brief summary of the article</p>
+                <hr />
+                <p>Start of article or article as a pdf?</p>
             </div>
         </>
     )
