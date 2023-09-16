facae341ec068145dfea7e3a9cb79bb752b8dbed
David Kral <dockral@gmail.com>
2023-09-15 20:46:51 -0700

    Added new files.

--- backend/app/models/paper.py
+++ backend/app/models/paper.py
@@ -5,7 +5,7 @@ from app.models.associations import Base
 
 class Paper(Base):
     __tablename__ = "papers"
-
+#This is the paper model. It contains the paper's ID, path to the PDF file, and the date and time when the paper was created and last updated.
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     pdf_path = Column(String(255), index=True)
     created_at = Column(DateTime, default=datetime.utcnow)
--- backend/app/models/topic.py
+++ backend/app/models/topic.py
@@ -6,7 +6,7 @@ from app.models.associations import Base
 
 class Topic(Base):
     __tablename__ = "topics"
-
+#This is the topic model. It contains the topic's ID, name, and the date and time when the topic was created and last updated.
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(255), index=True)
     created_at = Column(DateTime, default=datetime.utcnow)
