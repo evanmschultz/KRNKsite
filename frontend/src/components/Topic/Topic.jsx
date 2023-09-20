232038c2575fa718233a84218cd6586e4d7f42ab
David Kral <dockral@gmail.com>
2023-09-19 19:01:43 -0700

    Compressed edit routes, added route for papersByTopic, updates, topicsAndPapersByUser

--- frontend/src/components/Topic/Topic.jsx
+++ frontend/src/components/Topic/Topic.jsx
@@ -5,27 +5,38 @@ import { useParams } from "react-router-dom";
 import styles from "./Topic.module.css";
 
 const Topic = (props) => {
-    const {id} = useParams();
+    const { id } = useParams();
     const [papers, setPapers] = useState([]);
+    const [topic, setTopic] = useState({ name: "" }); // Initialize with an empty name
+
     useEffect(() => {
-        // TODO: change this axios call to be one that gets a topic AND the papers for a given topic
-        axios.get('http://localhost:8000/api/papers/')
-            .then(res => {
-                setPapers(res.data)
+        // Fetch the topic's name
+        axios.get(`http://localhost:8000/api/topic/${id}`/*, { withCredentials: true }*/)
+            .then((res) => {
+                setTopic({ name: res.data.name });
             })
-            .catch(err => console.log(err))
-    }, [])
+            .catch((err) => console.log(err));
+
+        // Fetch papers for the topic
+        axios.get(`http://localhost:8000/api/papers-by-topic/${id}`)
+            .then((res) => {
+                setPapers(res.data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
         <>
-            <Navbar></Navbar>
+            <Navbar />
             <div className={styles.content}>
-                <h1>Articles on **Topic Name**</h1>
+                <h1>Articles on {topic.name}</h1>
                 {papers.map((paper, idx) => {
                     return (
                         <div key={idx}>
                             <h3>{paper.title}</h3>
+                            <a href = {paper.pdf_url}>See the Paper!</a>
                         </div>
-                    )
+                    );
                 })}
             </div>
         </>
--- frontend/src/components/UserCard/UserCard.jsx
+++ frontend/src/components/UserCard/UserCard.jsx
@@ -4,46 +4,88 @@ import { useNavigate, useParams } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import AuthContext from "../Context/AuthContext";
 import styles from "./UserCard.module.css";
-import { Button, Table, TableBody, TableRow, TableCell, TableContainer } from "@mui/material";
+import {Button, TextField } from "@mui/material";
 
 const UserCard = (props) => {
-    const {id} = useParams();
+    const { id } = useParams();
     const [user, setUser] = useState({
         id: id,
-		first_name: "",
-		last_name: "",
-		email: "",
-		topics: [],
-		created_at: new Date().toDateString()
-	});
+        first_name: "",
+        last_name: "",
+        email: "",
+        topics: [],
+        created_at: new Date().toDateString(),
+    });
+
+    const [currentPassword, setCurrentPassword] = useState(""); // State for current password
+    const [editedEmail, setEditedEmail] = useState(""); // State for edited email
+    const [editedPassword, setEditedPassword] = useState(""); // State for edited password
 
     const navigate = useNavigate();
+
     useEffect(() => {
         const fetchUser = async () => {
-            const res = await axios.get('http://localhost:8000/api/user/' + id);
+            const res = await axios.get("http://localhost:8000/api/user/" + id);
             const data = await res.data;
-            data.created_at = new Date(data.created_at).toLocaleDateString()
+            data.created_at = new Date(data.created_at).toLocaleDateString();
             setUser(data);
-        }
+        };
         fetchUser().catch((err) => console.log(err));
     }, []);
 
+    const updateUser = async () => {
+        try {
+            // Send a PATCH request to update the user's information
+            await axios.patch(`http://localhost:8000/api/user/${id}/update`, {
+                email: editedEmail, // Add the email field for email updates
+                current_password: currentPassword,  // Add the current password field for password updates
+                password: editedPassword, // Add the password field for password updates
+            });
+            // Update the user state with the new email
+            setUser({ ...user, email: editedEmail });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <>
-        <Navbar />
+            <Navbar />
             <div className={styles.content}>
                 <h1>{user.first_name + " " + user.last_name}</h1>
                 <p>Member since: {user.created_at}</p>
-                {/* <TableContainer>
-                    <Table>
-                        <TableBody> 
-                            <TableRow>
-                                <TableCell style={{textAlign: "center"}}>Test</TableCell>
-                                <TableCell style={{textAlign: "center"}}>Test</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer> */}
+
+                {/* Display the current email */}
+                <p>Email: {user.email}</p>
+
+                {/* Input field to edit email */}
+                <TextField
+                    label="Edit Email"
+                    value={editedEmail}
+                    onChange={(e) => setEditedEmail(e.target.value)}
+                    variant="outlined"
+                />
+
+                <TextField
+                    label="Current Password"
+                    value={currentPassword}
+                    onChange={(e) => setCurrentPassword(e.target.value)}
+                    variant="outlined"
+                    type="password"
+                />
+                {/* Input field to edit password */}
+                <TextField
+                    label="Edit Password"
+                    value={editedPassword}
+                    onChange={(e) => setEditedPassword(e.target.value)}
+                    variant="outlined"
+                    type="password"
+                />
+
+                {/* Button to update user information */}
+                <Button onClick={updateUser} variant="contained" color="primary">
+                    Update
+                </Button>
             </div>
         </>
     );
